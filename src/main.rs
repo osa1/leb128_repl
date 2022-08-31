a052e72a591c83e328511170aff185b78ee6ff2f
Ömer Sinan Ağacan <omeragacan@gmail.com>
2022-08-31 09:38:37 +0200

    Simplify the implementation

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use leb128;
-
-use std::io::{self, Write};
-use std::str;
+use std::fmt::Write;
 
 fn display(bytes: &[u8]) -> String {
-    let mut s = vec![];
+    let mut s = String::new();
 
-    // Base 10.
-    write!(&mut s, "{:?}\n", bytes).unwrap();
+    // Base 10
+    writeln!(&mut s, "{:?}", bytes).unwrap();
 
-    // Hex.
+    // Hex
     write!(&mut s, "[").unwrap();
     for (i, b) in bytes.iter().enumerate() {
         if i != 0 {
@@ -19,7 +16,7 @@ fn display(bytes: &[u8]) -> String {
     }
     writeln!(&mut s, "]").unwrap();
 
-    // Binary.
+    // Binary
     write!(&mut s, "[").unwrap();
     for (i, b) in bytes.iter().enumerate() {
         if i != 0 {
@@ -29,7 +26,7 @@ fn display(bytes: &[u8]) -> String {
     }
     writeln!(&mut s, "]").unwrap();
 
-    String::from_utf8(s).unwrap()
+    s
 }
 
 fn main() {
@@ -42,8 +39,6 @@ base-10, hex, and binary.
 "
     );
 
-    let mut stdout = io::stdout();
-
     let mut rl = rustyline::Editor::<()>::with_config(
         rustyline::config::Builder::new()
             .max_history_size(usize::MAX)
@@ -52,44 +47,35 @@ base-10, hex, and binary.
     );
 
     loop {
-        let readline = rl.readline("> ");
-        let line = match readline {
+        let line = match rl.readline("> ") {
             Err(_) => break,
             Ok(line) => line,
         };
 
-        let buf = line.as_bytes();
+        let line = line.trim();
 
-        let uleb = str::from_utf8(&buf)
-            .ok()
-            .and_then(|s| s.trim().parse().ok())
-            .and_then(|n: u64| {
+        match line.parse::<u64>() {
+            Ok(u64) => {
                 let mut s = vec![];
-                leb128::write::unsigned(&mut s, n).ok()?;
-                Some(display(&s))
-            })
-            .unwrap_or_else(|| "error\n".into());
-        stdout
-            .write_all(b"# unsigned LEB128\n")
-            .and_then(|_| stdout.write_all(uleb.as_bytes()))
-            .and_then(|_| stdout.write_all(b"\n"))
-            .expect("failed to write to stdout");
+                leb128::write::unsigned(&mut s, u64).unwrap();
+                println!("# unsigned LEB128");
+                println!("{}", display(&s));
+            }
+            Err(err) => {
+                println!("Input is not u64: {}", err);
+            }
+        }
 
-        let leb = str::from_utf8(&buf)
-            .ok()
-            .and_then(|s| s.trim().parse().ok())
-            .and_then(|n: i64| {
+        match line.parse::<i64>() {
+            Ok(i64) => {
                 let mut s = vec![];
-                leb128::write::signed(&mut s, n).ok()?;
-                Some(display(&s))
-            })
-            .unwrap_or_else(|| "error\n".into());
-        stdout
-            .write_all(b"# signed LEB128\n")
-            .and_then(|_| stdout.write_all(leb.as_bytes()))
-            .and_then(|_| stdout.write_all(b"\n"))
-            .expect("failed to write to stdout");
-
-        stdout.flush().expect("failed to flush stdout");
+                leb128::write::signed(&mut s, i64).unwrap();
+                println!("# signed LEB128");
+                println!("{}", display(&s));
+            }
+            Err(err) => {
+                println!("Input is not i64: {}", err);
+            }
+        }
     }
 }
